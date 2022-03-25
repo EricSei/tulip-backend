e65395d48585795f9aee19ad0f10d5f0e87186db
unknown <awm21@pitt.edu>
2022-03-25 14:55:12 -0400

    implementations for errors

--- CapstoneProject/src/main/java/com/cognixia/jump/exception/NoSuchUserException.java
+++ CapstoneProject/src/main/java/com/cognixia/jump/exception/NoSuchUserException.java
@@ -1,5 +1,18 @@
 package com.cognixia.jump.exception;
 
-public class NoSuchUserException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value=HttpStatus.NOT_FOUND)
+public class NoSuchUserException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public NoSuchUserException(String msg) {
+		super(msg);
+	}
 
 }
